690502f4bbdcfbf07e248c6e55c548abf16c9ac9
keoki33 <gwong323@yahoo.com>
2019-03-14 00:04:36 +0000

    done

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize(name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@count += 1
+@@genres << genre
+@@artists << artist
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+@@genres.uniq
+end
+
+def self.artists
+@@artists.uniq
+end
+
+def self.genre_count
+  hash = Hash.new(0)
+@@genres.each do |genre|
+  hash[genre] += 1
+end
+hash
+end
+
+def self.artist_count
+  hash = Hash.new(0)
+@@artists.each do |artist|
+  hash[artist] += 1
+end
+hash
+end
+
+
+end
